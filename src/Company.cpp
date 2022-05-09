978717f6a5b20a17362504a60869b070ed5257f8
Abdulrahman Hassan Shawer <103496761+Abdulrahman956@users.noreply.github.com>
2022-05-09 19:05:57 +0200

    Update Company.cpp

--- src/Company.cpp
+++ src/Company.cpp
@@ -1,5 +1,7 @@
 #include "Company.h"
-
+#include<iostream>
+#include<string>
+using namespace std;
 Company::Company()
 {
     //ctor
@@ -8,4 +10,18 @@ Company::Company()
 Company::~Company()
 {
     //dtor
+}
+string Company::print()
+{
+     Customer::print();
+     cout<<"company name is: "<< company_name;
+     cout<<"location of company is: "<< location;
+}
+string Company::read()
+{
+    Customer::read();
+    cout<<"Enter your company name: ";
+    cin>>company_name ;
+    cout<<"Enter location of company: ";
+    cin>>location;
 }
