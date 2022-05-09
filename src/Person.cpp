08b585b15635f992fb6da8277e0b470dba657929
Abdulrahman Hassan Shawer <103496761+Abdulrahman956@users.noreply.github.com>
2022-05-09 19:04:42 +0200

    Update Person.cpp

--- src/Person.cpp
+++ src/Person.cpp
@@ -1,5 +1,6 @@
 #include "Person.h"
-
+#include<iostream>
+using namespace std;
 Person::Person()
 {
     //ctor
@@ -8,4 +9,18 @@ Person::Person()
 Person::~Person()
 {
     //dtor
+}
+string Person::read()
+{
+    Customer::read();
+    cout<<"Enter your name: ";
+    cin>>name;
+    cout<<"Enter your address: ";
+    cin>>address;
+}
+string Person::print()
+{
+    Customer::print();
+    cout<<"Name is: "<<name;
+    cout<<"Address is: "<<address;
 }
