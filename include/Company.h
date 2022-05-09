2e28531fe45795674af6a4e34a8890efb00bdb99
Abdulrahman Hassan Shawer <103496761+Abdulrahman956@users.noreply.github.com>
2022-05-09 19:03:45 +0200

    Update Company.h

--- include/Company.h
+++ include/Company.h
@@ -1,16 +1,21 @@
 #ifndef COMPANY_H
 #define COMPANY_H
+#include<string>
+#include"Customer.h"
+using namespace std;
 
-
-class Company
+class Company : public Customer
 {
     public:
         Company();
         virtual ~Company();
-
+        string print();
+        string read();
     protected:
 
     private:
+        string location;
+        string company_name;
 };
 
 #endif // COMPANY_H
