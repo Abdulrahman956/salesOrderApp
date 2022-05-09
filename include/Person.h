ba607ff4695ddfd4338e4d25e70aa5b1123ceab6
Abdulrahman Hassan Shawer <103496761+Abdulrahman956@users.noreply.github.com>
2022-05-09 19:02:43 +0200

    Update Person.h

--- include/Person.h
+++ include/Person.h
@@ -1,16 +1,21 @@
+#include"Customer.h"
 #ifndef PERSON_H
 #define PERSON_H
+#include<string>
+using namespace std;
 
-
-class Person
+class Person : public Customer
 {
     public:
         Person();
         virtual ~Person();
-
+        string read();
+        string print();
     protected:
 
     private:
+        string address;
+        string name;
 };
 
 #endif // PERSON_H
