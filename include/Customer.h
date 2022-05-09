3bf420ae7e53bd49539fd48fa3aeebf7043855e9
Abdulrahman Hassan Shawer <103496761+Abdulrahman956@users.noreply.github.com>
2022-05-09 18:33:02 +0200

    Update Customer.h

--- include/Customer.h
+++ include/Customer.h
@@ -1,16 +1,21 @@
 #ifndef CUSTOMER_H
 #define CUSTOMER_H
-
-
+#include<string>
+using namespace std;
 class Customer
 {
     public:
         Customer();
+        Customer(int ,string);
         virtual ~Customer();
+        virtual string print();
+        virtual string read();
 
     protected:
-
+        int c_id;
+        string c_phone;
     private:
+
 };
 
 #endif // CUSTOMER_H
